b88c628af7c563825783bfe78707fd79b6d154cf
Aleksey Kuznietsov <utilmind@gmail.com>
2024-10-06 10:43:29 -0400

    Create inc_php_var.php

---
+++ inc_php_var.php
@@ -0,0 +1,27 @@
+<?php
+#
+# This script increase some variable in another PHP script.
+# Internal, but feel free to modify it for your needs.
+#
+if (!is_array($argv) || ($i = count($argv)) < 3) {
+    echo <<<END
+Usage: inc_php_var.php [filename.php] [\$var] [prefix (optionally, can be skipped)]
+Example: inc_php_var.php .credentials.php \$ext_script_ver v=
+
+END;
+    exit;
+}
+
+if ($content = file_get_contents($argv[1])) {
+
+    $content = preg_replace_callback('/('.preg_quote($argv[2]).'\s*?=\s*?(\'|")'.preg_quote($argv[3] ?? '').')(.*?)(\'|")/s', function($m) {
+               ++$m[3];
+               return "$m[1]$m[3]$m[4]";
+           }, $content);
+
+    file_put_contents($argv[1], $content);
+    echo "$argv[1] updated.\n";
+
+}else {
+    echo "$argv[1] not found.\n";
+}
